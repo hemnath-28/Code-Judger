18eacaf2117bc83a692a974c3488f42a71a25f89
hemnath-28 <hemnath0628@gmail.com>
2026-07-04 12:36:04 +0530

    Code Running Contorller Logic and Routes

---
+++ backend/src/modules/routes/coding.routes.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { validateRequest } from '../../middleware/validateRequest.js';
+import {
+  listProblems,
+  getProblem,
+  listSubmissions,
+  runBatch,
+  runCode,
+  submitCode
+} from '../controllers/coding.controller.js';
+import {
+  problemIdParamSchema,
+  runBatchSchema,
+  runSchema,
+  submitSchema
+} from '../utils/validation.js';
+
+const router = Router();
+
+router.get('/problems', listProblems);
+router.get('/problems/:problemId', validateRequest(problemIdParamSchema), getProblem);
+router.post('/run', validateRequest(runSchema), runCode);
+router.post('/run/batch', validateRequest(runBatchSchema), runBatch);
+router.post('/submit', validateRequest(submitSchema), submitCode);
+router.get('/submissions', listSubmissions);
+
+export default router;
